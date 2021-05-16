abff8a2f54e2a8e14701d40069419a4c731fb049
root <root@ip-172-31-23-64.ec2.internal>
2021-05-16 16:20:15 +0000

    subida test

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 # Script para lanzar un contenedor docker con persistencia de datos. Empezamos instalado y habilitando docker.
 apt update
-apt install docker
+apt install docker -y
 systemctl enable docker
 systemctl start docker 
 # Especificamos un nombre y la versión de apache para utilizar.
