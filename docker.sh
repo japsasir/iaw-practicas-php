493c87380bcbf71072c975e1a19eaa5dea847c82
japsasir <japsasir@gmail.com>
2021-05-16 13:53:10 +0200

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,7 @@
+# Script para lanzar un contenedor docker con persistencia de datos. Empezamos instalado y habilitando docker.
+apt update
+apt install docker
+systemctl enable docker
+systemctl start docker 
+# Especificamos un nombre y la versión de apache para utilizar.
+docker run -d --rm --name my-apache-php-app -p 80:80 -v "$PWD":/var/www/html php:7.4-apache
